a9ac382e3903b18109c443f74c3b2541fb911d0b
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-11-26 20:07:22 +0330

    Configure one way SSL using reusable bundle from Spring Boot 3.3.6 and Java 21

--- src/main/java/my/training/config/Config.java
+++ src/main/java/my/training/config/Config.java
@@ -1,39 +1,16 @@
 package my.training.config;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
 
 @Configuration
 public class Config {
 
-    @Value("${ssl.keystore.type}")
-    private String keyStoreType;
-
-    @Value("${ssl.truststore.path}")
-    private String trustStorePath;
-
-    @Value("${ssl.truststore.password}")
-    private String trustStorePassword;
-
     @Bean
-    public RestTemplate restTemplate() throws Exception {
-        SSLContext sslContext = SSLContextBuilder.create()
-                .setKeyStoreType(keyStoreType)
-                .loadTrustMaterial(ResourceUtils.getFile(trustStorePath), trustStorePassword.toCharArray())
-                .build();
-
-        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return new RestTemplate(requestFactory);
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
+        return restTemplateBuilder.setSslBundle(sslBundles.getBundle("secure-client")).build();
     }
 }
--- src/main/java/my/training/service/GreetingService.java
+++ src/main/java/my/training/service/GreetingService.java
@@ -1,6 +1,6 @@
 package my.training.service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import my.training.dto.Greeting;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -8,15 +8,12 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
+@RequiredArgsConstructor
 public class GreetingService {
     private final RestTemplate restTemplate;
 
     @Value("${greeting.service.url}")
     private String greetingServiceUrl;
-
-    public GreetingService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
 
     public Greeting greet(String name) {
         return restTemplate.getForObject(
